884da95b65dfcbe299423f9820581c6e4ea05fb1
Brankovanov <g_brankovanov@abv.bg>
2018-05-09 11:02:39 +0300

    Update of arrays Exercise.

--- 2.ProgrammingFundamentals/03_Arrays/Exercise_06_MaxSequenceEqualElements/maxSequenceEqualElements.cs
+++ 2.ProgrammingFundamentals/03_Arrays/Exercise_06_MaxSequenceEqualElements/maxSequenceEqualElements.cs
@@ -6,6 +6,110 @@ namespace Exercise_06_MaxSequenceEqualElements
     {
         public static void Main(string[] args)
         {
+            Input();
+        }
+
+        static void Input()
+        {
+            var input = Console.ReadLine();
+
+            CreateArray(input);
+        }
+
+        static void CreateArray(string input)
+        {
+            var sequenceArray = input.Split(' ');
+
+            CompareSequence(sequenceArray);
+        }
+
+        static void CompareSequence(string[] sequenceArray)
+        {
+            var finalSequence = string.Empty;
+            var firstSequence = string.Empty;
+            var counter = sequenceArray.Length - 2;
+
+            for (var index = 0; index <= counter; index++)
+            {
+                if (sequenceArray[index].Equals(sequenceArray[index + 1]))
+                {
+                    firstSequence += sequenceArray[index].ToString();
+
+                    if (firstSequence.Length > finalSequence.Length)
+                    {
+                        finalSequence = firstSequence;
+
+                        if (index == counter)
+                        {
+                            if (finalSequence.Length > 0)
+                            {
+                                Print(finalSequence + finalSequence[0].ToString());
+                            }
+                            else
+                            {
+                                Console.Write(sequenceArray[0]);
+                            }
+                        }
+                    }
+                    else if (firstSequence.Length <= finalSequence.Length)
+                    {
+                        if (index == counter)
+                        {
+                            if (finalSequence.Length > 0)
+                            {
+                                Print(finalSequence + finalSequence[0].ToString());
+                            }
+                            else
+                            {
+                                Console.Write(sequenceArray[0]);
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    firstSequence = string.Empty;
+
+                    if (firstSequence.Length > finalSequence.Length)
+                    {
+                        finalSequence = firstSequence;
+
+                        if (index == counter)
+                        {
+                            if(finalSequence.Length>0)
+                            {
+                                Print(finalSequence + finalSequence[0].ToString());
+                            }
+                            else
+                            {
+                                Console.Write(sequenceArray[0]);
+                            }                           
+                        }
+                    }
+                    else if (firstSequence.Length <= finalSequence.Length)
+                    {
+                        if (index == counter)
+                        {
+                            if (finalSequence.Length > 0)
+                            {
+                                Print(finalSequence + finalSequence[0].ToString());
+                            }
+                            else
+                            {
+                                Console.Write(sequenceArray[0]);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        static void Print(string finalSequence)
+        {           
+            foreach (var ch in finalSequence)
+            {
+                Console.Write(ch + " ");
+            }
         }
     }
 }
---
+++ 2.ProgrammingFundamentals/03_Arrays/Exercise_07_MaxSequenceOfIncreasingElements/maxSequenceOfIncreasingElements.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercise_07_MaxSequenceOfIncreasingElements
+{
+    public class maxSequenceOfIncreasingElements
+    {
+        public static void Main(string[] args)
+        {
+            Input();
+        }
+
+        static void Input()
+        {
+            var input = Console.ReadLine();
+
+            CreateArray(input);
+        }
+
+        static void CreateArray(string input)
+        {
+            var mainArray = input.Split(' ').Select(x => int.Parse(x)).ToArray();
+
+            SortArray(mainArray);
+        }
+
+        static void SortArray(int[] mainArray)
+        {
+            var temporaryString = new List<int>();
+            var finalString = new List<int>();
+
+            for (var index = 0; index <= mainArray.Length - 2; index++)
+            {
+                if (mainArray[index + 1] - mainArray[index] >= 1)
+                {
+                    temporaryString.Add(mainArray[index]);
+
+                    if (index == mainArray.Length - 2)
+                    {
+                        if (finalString.Count >= temporaryString.Count)
+                        {
+                            temporaryString.RemoveRange(0, temporaryString.Count);
+                        }
+                        else if (finalString.Count < temporaryString.Count)
+                        {
+                            finalString.RemoveRange(0, finalString.Count);
+                            finalString.AddRange(temporaryString);
+                            temporaryString.RemoveRange(0, temporaryString.Count);
+                        }
+                    }
+                }
+                else
+                {
+                    if (finalString.Count >= temporaryString.Count)
+                    {
+                        temporaryString.RemoveRange(0, temporaryString.Count);
+                    }
+                    else if (finalString.Count < temporaryString.Count)
+                    {
+                        finalString.RemoveRange(0, finalString.Count);
+                        finalString.AddRange(temporaryString);
+                        temporaryString.RemoveRange(0, temporaryString.Count);
+                    }
+                }
+            }
+
+            Print(finalString);
+        }
+
+        static void Print(List<int> finalString)
+        {
+            foreach (var ch in finalString)
+            {
+                Console.Write(ch + " ");
+            }
+
+            Console.Write(int.Parse(finalString[finalString.Count - 1].ToString()) + 1);
+        }
+    }
+}

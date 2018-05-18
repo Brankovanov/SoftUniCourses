9253134a56112b19f878956d6680311d02272784
Brankovanov <g_brankovanov@abv.bg>
2018-05-18 21:46:16 +0300

    Dictionaries update.

--- 2.ProgrammingFundamentals/05_DictionariesLambdaLINQ/Exercise_05_HandsOfCards/handsOfCards.cs
+++ 2.ProgrammingFundamentals/05_DictionariesLambdaLINQ/Exercise_05_HandsOfCards/handsOfCards.cs
@@ -13,29 +13,163 @@ namespace Exercise_05_HandsOfCards
 
         static void ReceiveInput()
         {
-            List<string> listOfCards = new List<string>();
+            List<string> listOfPlayers = new List<string>();
+
             var input = Console.ReadLine();
-            listOfCards.Add(input);
+            listOfPlayers.Add(input);
 
             while (input != "JOKER")
             {
                 input = Console.ReadLine();
-                listOfCards.Add(input);
+                listOfPlayers.Add(input);
             }
 
-            listOfCards.Remove("JOKER");
-            CalculateResult(listOfCards);
+            listOfPlayers.Remove("JOKER");
+
+            CreateGameRecord(listOfPlayers);
         }
 
-        static void CalculateResult(List<string> listOfCards)
+        static void CreateGameRecord(List<string> listOfPlayers)
         {
-            Dictionary<string, int> resultOfTheGame = new Dictionary<string, int>();
-            List<string> listOfResults = new List<string>();
-            var result = 0;
+            Dictionary<string, int> gameRecord = new Dictionary<string, int>();
+            List<string> temp = new List<string>();
+            var name = string.Empty;
+            var finalPoints = 0;
+            var card = string.Empty;
 
-            foreach (var entry in listOfCards)
+            foreach (var player in listOfPlayers)
             {
-                listOfResults = entry.Split(' ', ',').ToList();
+                temp = player.Split(' ', ',').ToList();
+                temp.RemoveAll(X => X == "");
+                name = temp[0];
+
+                for (var index = 1; index <= temp.Count - 1; index++)
+                {
+                    card = temp[index];
+                    CalculatePoints(card, finalPoints, gameRecord, name);
+
+                    if (temp.Contains(card))
+                    {
+                        temp.RemoveAll(x => x == card);
+                        index -= 1;
+                    }
+
+                    card = string.Empty;
+                }
+
+                temp.RemoveRange(0, temp.Count);
+                name = string.Empty;
+                finalPoints = 0;
+            }
+
+            Print(gameRecord);
+        }
+
+        static void CalculatePoints(string card, int finalPoints, Dictionary<string, int> gameRecord, string name)
+        {
+            var points = 0;
+            card.Split();
+            var multiplier = 0;
+
+            switch (card[1])
+            {
+                case 'S':
+                    multiplier = 4;
+                    break;
+                case 'H':
+                    multiplier = 3;
+                    break;
+                case 'D':
+                    multiplier = 2;
+                    break;
+                case 'C':
+                    multiplier = 1;
+                    break;
+                case '0':
+                    switch (card[2])
+                    {
+                        case 'S':
+                            multiplier = 4;
+                            break;
+                        case 'H':
+                            multiplier = 3;
+                            break;
+                        case 'D':
+                            multiplier = 2;
+                            break;
+                        case 'C':
+                            multiplier = 1;
+                            break;
+                    }
+                    break;
+            }
+
+            switch (card[0])
+            {
+                case '1':
+                    points += (10 * multiplier);
+                    break;
+                case '2':
+                    points += (2 * multiplier);
+                    break;
+                case '3':
+                    points += (3 * multiplier);
+                    break;
+                case '4':
+                    points += (4 * multiplier);
+                    break;
+                case '5':
+                    points += (5 * multiplier);
+                    break;
+                case '6':
+                    points += (6 * multiplier);
+                    break;
+                case '7':
+                    points += (7 * multiplier);
+                    break;
+                case '8':
+                    points += (8 * multiplier);
+                    break;
+                case '9':
+                    points += (9 * multiplier);
+                    break;
+                case 'J':
+                    points += (11 * multiplier);
+                    break;
+                case 'Q':
+                    points += (12 * multiplier);
+                    break;
+                case 'K':
+                    points += (13 * multiplier);
+                    break;
+                case 'A':
+                    points += (14 * multiplier);
+                    break;
+            }
+
+            finalPoints += points;
+            FillTheDict(gameRecord, finalPoints, name);
+        }
+
+        static void FillTheDict(Dictionary<string, int> gameRecord, int finalPoints, string name)
+        {
+            if (gameRecord.ContainsKey(name))
+            {
+                gameRecord[name] += finalPoints;
+            }
+            else
+            {
+                gameRecord.Add(name, finalPoints);
+            }
+
+
+        }
+
+        static void Print(Dictionary<string, int> gameRecord)
+        {
+            foreach (var entry in gameRecord)
+            {
+                Console.WriteLine(entry.Key + " " + entry.Value);
             }
         }
     }
---
+++ 2.ProgrammingFundamentals/05_DictionariesLambdaLINQ/Exercise_06_UserLog/userLog.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercise_06_UserLog
+{
+    public class userLog
+    {
+        public static void Main(string[] args)
+        {
+            ReceiveInput();
+        }
+
+        static void ReceiveInput()
+        {
+            List<string> listOfInputs = new List<string>();
+
+            var input = Console.ReadLine();
+            listOfInputs.Add(input);
+
+            while (input != "end")
+            {
+                input = Console.ReadLine();
+                listOfInputs.Add(input);
+            }
+
+            listOfInputs.Remove("end");
+            CreateUsers(listOfInputs);
+        }
+
+        static void CreateUsers(List<string> listOfInputs)
+        {
+            List<string> tempList = new List<string>();
+            var userName = string.Empty;
+            var ipAdress = string.Empty;
+            var message = string.Empty;
+
+            SortedDictionary<string, Dictionary<string, int>> userLog = new SortedDictionary<string, Dictionary<string, int>>();
+
+            foreach (var entry in listOfInputs)
+            {
+                tempList = entry.Split(' ').ToList();
+
+                userName = tempList[2].Remove(0, 5);
+                message = tempList[1].Remove(0, 8);
+                ipAdress = tempList[0].Remove(0, 3);
+
+                if (!userLog.ContainsKey(userName))
+                {
+                    userLog[userName] = new Dictionary<string, int>();
+                }
+
+                if(!userLog[userName].ContainsKey(ipAdress))
+                {
+                    userLog[userName][ipAdress] = 0;
+                }
+
+                userLog[userName][ipAdress]++;
+            }
+
+            foreach (var user in userLog)
+            {
+                Console.WriteLine(user.Key + ": ");
+
+                foreach (var ip in user.Value)
+                {
+                    if (ip.Key != user.Value.Keys.Last())
+                    {
+                        Console.Write(ip.Key + " => " + ip.Value + ", ");
+                    }
+                    else
+                    {
+                        Console.Write(ip.Key + " => " + ip.Value + ". ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
+

3fd9356db5e6f9101417d8295133e9ea8e446051
Brankovanov <g_brankovanov@abv.bg>
2019-01-05 12:45:42 +0200

    C#

---
+++ 6.Software Technologies/Exam_Prep/C#/ProjectRider/Models/Project.cs
@@ -0,0 +1,14 @@
+﻿
+namespace ProjectRider.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Budget { get; set; }
+
+    }
+}

39648a863c29e00057a6969c63b19cfc7cfb8f07
Georgi Brankovanov <g_brankovanov@abv.bg>
2020-02-09 12:20:06 +0200

    A console App that draws specific logo on the console based on given size paramether.

---
+++ LogoBuilder/LogoBuilder/Builder.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Text;
+
+namespace LogoBuilder
+{
+    class Builder
+    {
+        private int logoSize;
+
+        public int LogoSize { get => logoSize; set => logoSize = value; }
+
+        //Merges the upper and the lower parts of the logo together and returns the final finished version of the logo.
+        public string Build()
+        {
+            var logo = string.Empty;
+            logo += BuilldUpper();
+            logo += BuildLower();
+
+            return (logo);
+        }
+
+        //Builds the lower half of the logo using StringBuilder and returns it to LBuilder method.
+        private string BuildLower()
+        {
+            var demo = string.Empty;
+            var sb = new StringBuilder();
+
+            sb.Append('-', (int)Math.Floor((double)this.LogoSize / 2))
+              .Append('*', this.LogoSize)
+              .Append('-', 1)
+              .Append('*', this.LogoSize * 2 - 1)
+              .Append('-', 1)
+              .Append('*', this.LogoSize)
+              .Append('-', (int)Math.Floor((double)this.LogoSize / 2))
+              .Append('-', (int)Math.Floor((double)this.LogoSize / 2))
+              .Append('*', this.LogoSize)
+              .Append('-', 1)
+              .Append('*', this.LogoSize * 2 - 1)
+              .Append('-', 1)
+              .Append('*', this.LogoSize)
+              .Append('-', (int)Math.Floor((double)this.LogoSize / 2))
+              .Append(Environment.NewLine);
+
+            for (var row = 1; row < Math.Ceiling((double)this.LogoSize / 2); row++)
+            {
+                sb.Append('-', (int)Math.Floor((double)this.LogoSize / 2) - row)
+                  .Append('*', this.LogoSize)
+                  .Append('-', 2 * row + 1)
+                  .Append('*', (this.LogoSize * 2 - 1) - row * 2)
+                  .Append('-', 2 * row + 1)
+                  .Append('*', this.LogoSize)
+                  .Append('-', (int)Math.Floor((double)this.LogoSize / 2) - row)
+                  .Append('-', (int)Math.Floor((double)this.LogoSize / 2) - row)
+                  .Append('*', this.LogoSize)
+                  .Append('-', 2 * row + 1)
+                  .Append('*', (this.LogoSize * 2 - 1) - row * 2)
+                  .Append('-', 2 * row + 1)
+                  .Append('*', this.LogoSize)
+                  .Append('-', (int)Math.Floor((double)this.LogoSize / 2) - row)
+                  .Append(Environment.NewLine);
+            }
+
+            demo += sb.ToString();
+
+            return demo;
+        }
+
+        //Builds the upper half of the logo using StringBuilder and returns it to the Build method.
+        private string BuilldUpper()
+        {
+            var demo = string.Empty;
+            var sb = new StringBuilder();
+
+            sb.Append('-', this.LogoSize)
+              .Append('*', this.LogoSize)
+              .Append('-', this.LogoSize)
+              .Append('*', this.LogoSize)
+              .Append('-', this.LogoSize)
+              .Append('-', this.LogoSize)
+              .Append('*', this.LogoSize)
+              .Append('-', this.LogoSize)
+              .Append('*', this.LogoSize)
+              .Append('-', this.LogoSize)
+              .Append(Environment.NewLine);
+
+            for (var row = 1; row < Math.Ceiling((double)this.LogoSize / 2); row++)
+            {
+                sb.Append('-', this.LogoSize - row)
+                  .Append('*', this.LogoSize + 2 * row)
+                  .Append('-', this.LogoSize - 2 * row)
+                  .Append('*', this.LogoSize + 2 * row)
+                  .Append('-', this.LogoSize - row)
+                  .Append('-', this.LogoSize - row)
+                  .Append('*', this.LogoSize + 2 * row)
+                  .Append('-', this.LogoSize - 2 * row)
+                  .Append('*', this.LogoSize + 2 * row)
+                  .Append('-', this.LogoSize - row)
+                  .Append(Environment.NewLine);
+            }
+
+            demo += sb.ToString();
+
+            return demo;
+        }
+
+        public Builder(int size)
+        {
+            this.LogoSize = size;
+        }
+    }
+}
---
+++ LogoBuilder/LogoBuilder/StartUp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace LogoBuilder
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            ReceiveLogoDimention();
+        }
+
+        //Receives an integer that represents the size of the logo.
+        private static void ReceiveLogoDimention()
+        {
+            var logoSize = int.Parse(Console.ReadLine());
+
+            CreateLogo(logoSize);
+        }
+
+        //Creates a LogoBuilder object that is used to build the logo.
+        private static void CreateLogo(int logoSize)
+        {
+            var builder = new Builder(logoSize);
+            var finalLogo = builder.Build();
+
+            PrintLogo(finalLogo);
+        }
+
+        //Prints the final version of the logo.
+        private static void PrintLogo(string finalLogo)
+        {
+            Console.WriteLine(finalLogo);
+        }
+    }
+}
